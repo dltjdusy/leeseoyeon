613fae8357d09317863559c91f7c3bb3ac0c4844
dltjdusy <jksy9118@naver.com>
2025-05-12 10:47:31 +0900

    Update views.py

--- account/views.py
+++ account/views.py
@@ -1,3 +1,22 @@
+from .models import Blog
+from .serializers import BlogSerializer
+from rest_framework import viewsets
+
+class BlogViewSet(viewsets.ModelViewSet):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user = self.request.user)
+
 from django.shortcuts import render
+from .serializers import UserSerializer
+from .models import User
+from rest_framework import generics
+
+# 회원가입
+class UserCreate(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 # Create your views here.
