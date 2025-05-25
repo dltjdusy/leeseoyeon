6188ff5b510e93846d8cbab26eecc42c9f8c6be5
dltjdusy <jksy9118@naver.com>
2025-05-26 02:03:53 +0900

    Update views.py

--- account/views.py
+++ account/views.py
@@ -26,3 +26,15 @@ class PostViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_class = PostFilter
     permission_classes = [IsSuperUserOrReadOnly]
+
+from rest_framework import viewsets, filters
+from .models import Post
+from .serializers import PostSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'content']
